6c6e9129160caa44eb6a1f7d930e65e7b7035588
Faisal <faisalkingffz@gmail.com>
2025-03-23 19:29:08 +0530

    Initial commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -15,6 +15,9 @@ const sequelize = new Sequelize(MYSQL_DATABASE, MYSQL_USER, MYSQL_PASSWORD, {
   host: MYSQL_HOST,
   port: MYSQL_PORT || 3306,
   dialect: 'mysql',
+  dialectOptions: {
+    connectTimeout: 10000, // increase timeout to 10 seconds
+  },
   logging: false, // set to console.log to see SQL queries
 });
 
